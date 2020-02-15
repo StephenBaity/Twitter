45a6929cd87789d4998793f74de8a00252a28b32
Stephen Baity <steve.baity@gmail.com>
2020-02-14 23:50:29 -0800

    Fixed all unwrapping issues.

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -11,19 +11,24 @@ import UIKit
 
 class HomeTableTableViewController: UITableViewController {
 
-	var tweetArray = [NSDictionary()]
+	var tweetArray = [NSDictionary]()
 	var numberOfTweets : Int!
+	
+	let myRefreshControl = UIRefreshControl()
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		loadTweets()
+		
+		myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+		tableView.refreshControl = myRefreshControl
 
     }
 	
-	func loadTweets () {
+	@objc func loadTweets () {
 		
 		let url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-		let params = ["count": 10]
+		let params = ["count": 20]
 		TwitterAPICaller.client?.getDictionariesRequest(url: url, parameters: params, success: { (tweets : [NSDictionary]) in
 			
 			self.tweetArray.removeAll()
@@ -32,6 +37,7 @@ class HomeTableTableViewController: UITableViewController {
 			}
 			
 			self.tableView.reloadData()
+			self.myRefreshControl.endRefreshing()
 			
 		}, failure: { (Error) in
 			print("Could not retrieve tweet")
@@ -47,9 +53,16 @@ class HomeTableTableViewController: UITableViewController {
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
 		
-		let user = tweetArray[indexPath.row]["user"] as? NSDictionary
+		let user = tweetArray[indexPath.row]["user"] as! NSDictionary
 		cell.tweetContentLabel.text = tweetArray[indexPath.row]["text"] as? String
-		cell.usernameLabel.text = user?["name"] as? String
+		cell.usernameLabel.text = user["name"] as? String
+		
+		let imageUrl = URL(string: user["profile_image_url_https"] as! String)
+		let data = try? Data(contentsOf: imageUrl!)
+		
+		if let imageData = data {
+			cell.profileImageView.image = UIImage(data: imageData)
+		}
 		
 		
 		return cell
